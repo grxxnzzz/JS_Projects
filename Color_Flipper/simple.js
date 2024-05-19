8c657ed9d4a1b30dbf6bd91bcc5a8bc47c373992
grxxndxxm <gkrabsti@gmail.com>
2024-05-19 17:05:06 +0300

    adding first project "color flipper"

---
+++ Color_Flipper/simple.js
@@ -0,0 +1,14 @@
+const colors = ["green", "red", "rgba(133,122,200)", "#f15025"]
+const btn = document.getElementById("btn");
+const color = document.querySelector(".color");
+
+const getRandomNumber = () => {
+  return Math.floor(Math.random(0, 3) * colors.length);
+}
+
+btn.addEventListener('click', () => {
+    const randomNumber = getRandomNumber();
+    console.log(randomNumber);
+    document.body.style.backgroundColor = colors[randomNumber];
+    color.textContent = colors[randomNumber];
+})
